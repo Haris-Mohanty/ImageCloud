509b6c64d3f701910377ef36100fe59fb0f7dcfd
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-14 15:31:17 +0530

    script.js - Signup - ajax request processing..

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
   $(".signup-form").submit(function (e) {
     e.preventDefault();
     let formData = new FormData();
+    //ajax request
     
+
   });
 });
 // SIGNUP CODE END
