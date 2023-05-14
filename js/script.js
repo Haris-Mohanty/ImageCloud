ad497086b1ffced4bb79e993409bff7f569d07db
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-14 15:27:54 +0530

    script.js - Signup - Form data prepare successfu;lly.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // SIGNUP CODE START
+$(document).ready(function () {
+  $(".signup-form").submit(function (e) {
+    e.preventDefault();
+    let formData = new FormData();
+    
+  });
+});
+// SIGNUP CODE END
