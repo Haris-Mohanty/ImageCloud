15b8669c5ca90276bf05d8ebe5c49bd271607aee
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-18 22:11:53 +0530

    ImageCloud :- Profile page - Upload pic - ajax request processing.......

--- profile/profile.js
+++ profile/profile.js
@@ -14,5 +14,12 @@ logoutBtn.addEventListener("click", () => {
 // LOGOUT CODE END
 
 // UPLOAD PIC CODE START
-
+let uploadInput = document.querySelector(".upload-input");
+uploadInput.addEventListener("change", () => {
+  let uploadBtn = document.querySelector(".upload-btn");
+  uploadBtn.addEventListener("click", () => {
+    // ajax request
+    $.ajax({});
+  });
+});
 // UPLOAD PIC CODE END
