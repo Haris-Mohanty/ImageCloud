506b6b570e32269a73039409d96ca45c67cb9aab
Haris-Mohanty <108195360+Haris-Mohanty@users.noreply.github.com>
2023-06-07 11:18:15 +0530

    Update database.php

--- Common_files/php/database.php
+++ Common_files/php/database.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$db = new mysqli("localhost", "root", "865876@", "imagecloud");
+$db = new mysqli("localhost", "root", "******", "imagecloud");
 
 if($db -> connect_error){
     echo "Database Not connected!";
